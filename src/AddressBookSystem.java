6efc5f6554b4d437924349112b668a9a0029a106
NCSaketh <nc.saketh1@gmail.com>
2020-09-30 02:24:55 +0530

    UC2:adding a new contact

--- src/AddressBookSystem.java
+++ src/AddressBookSystem.java
@@ -2,26 +2,49 @@
 
 import java.util.*;
 class Contact {
+	Scanner sc=new Scanner(System.in);
 	String first,last,address,String,city,state,zip,phno,email;
-	ArrayList <Contact> c=new ArrayList<Contact>();
-	public Contact(String first,String last,String address,String city,String state,String zip,String phno,String email)
+	public void addContact()
 	{
-		this.first=first;
-		this.last=last;
-		this.address=address;
-		this.city=city;
-		this.state=state;
-		this.zip=zip;
-		this.phno=phno;
-		this.email=email;
+
+		System.out.println("Enter First Name");
+		this.first=sc.next();
+		System.out.println("Enter Last Name");
+		this.last=sc.next();
+		System.out.println("Enter Address");
+		this.address=sc.next();
+		System.out.println("Enter city");
+		this.city=sc.next();
+		System.out.println("Enter state");
+		this.state=sc.next();
+		System.out.println("Enter zip code");
+		this.zip=sc.next();
+		System.out.println("Enter phone number");
+		String phno=sc.next();
+		System.out.println("Enter email");
+		String email=sc.next();
 	}
 }
 public class AddressBookSystem {
 
 	public static void main(String args[])
 	{
+		ArrayList <Contact> c=new ArrayList<Contact>();
 		System.out.println("Welcome to Address Book program");
 		Scanner sc=new Scanner(System.in);
 		
+		int num=0;
+		while(num !=2)
+		{
+			System.out.println("1.ADD A CONTACT DETAIL");			
+			System.out.println("2.EXIT");
+			num=sc.nextInt();
+			if(num==1)
+			{
+				Contact a=new Contact();
+				a.addContact();
+				
+			}
+		}
 	}
 }
