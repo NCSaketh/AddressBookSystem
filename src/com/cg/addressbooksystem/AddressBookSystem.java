2e5b62a23f459619ec1cf51395d4eadc919e5b2e
NCSaketh <nc.saketh1@gmail.com>
2020-10-12 01:39:24 +0530

    UC4: Deleting contact of a person

--- src/com/cg/addressbooksystem/AddressBookSystem.java
+++ src/com/cg/addressbooksystem/AddressBookSystem.java
@@ -11,11 +11,13 @@ public class AddressBookSystem {
 		Scanner sc=new Scanner(System.in);
 		
 		int num=0;
-		while(num !=4)
+		while(num !=5)
 		{
 			System.out.println("1.ADD A CONTACT DETAIL");
 			System.out.println("2.EDIT EXISTING CONTACT USING NAME");
-			System.out.println("3.VIEW CONTACT DETAILS USING NAME OR EXIT");
+			System.out.println("3.VIEW CONTACT DETAILS USING NAME ");
+			System.out.println("4.DELETE THE CONTACT OF A PERSON");
+			System.out.println("5.EXIT");
 			num=sc.nextInt();
 			if(num==1)
 			{
@@ -69,6 +71,30 @@ public class AddressBookSystem {
 				else
 				{
 					contact.get(i).viewContact();
+				}
+			}
+
+			if(num==4)
+			{
+				System.out.println("Enter the First Name");
+				String fn=sc.next();
+				System.out.println("Enter the Last Name");
+				String ln=sc.next();
+				int i=0,j=0;
+				for(i=0;i<contact.size();i++)
+				{
+					if(contact.get(i).first_name.equalsIgnoreCase(fn) && contact.get(i).last_name.equalsIgnoreCase(ln))
+					{
+						j=1;
+						contact.remove(i);
+						System.out.println("Contact is Deleted");
+						break;
+					}
+				}
+				if(j==0)
+				{
+					System.out.println("No Contact found");
+
 				}
 			}
 		}
